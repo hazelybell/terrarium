1eff984e10f7afa6e888516b73755dbc852187bb
Hazel Victoria Campbell <github@orezpraw.com>
2019-03-09 11:51:28 -0700

    fix refactor

--- terrarium.py
+++ terrarium.py
@@ -245,7 +245,7 @@ if __name__=="__main__":
     scheduler = Scheduler()
     morse = Morse(scheduler)
     hb = Heartbeat(scheduler)
-    Outlet(TIME_ON, TIME_OFF, hb)
+    Outlet(time_on=TIME_ON, time_off=TIME_OFF, hb)
     morse.morse("start")
     scheduler.loop()
     print("Ran out of things to do, exiting")
