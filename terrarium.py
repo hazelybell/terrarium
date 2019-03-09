8ed96c8e93c7609c2f41acd23d988c27e7992271
Hazel Victoria Campbell <github@orezpraw.com>
2019-03-09 10:42:24 -0700

    attempt to refactor

--- terrarium.py
+++ terrarium.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import time
 import datetime
+import math
 
 import automationhat
 
@@ -24,43 +25,176 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     '?':'..--..', '/':'-..-.', '-':'-....-',
                     '(':'-.--.', ')':'-.--.-', ' ':' '}
 
-def morse(msg):
-    msg = msg.upper()
-    morsed = "  "
-    for i in range(0, len(msg)):
-        morsed = morsed + MORSE_CODE_DICT[msg[i]]
-        if i + 1 < len(msg) and msg[i+1] != ' ':
-            morsed = morsed + '_'
-        elif i + 1 >= len(msg):
-            morsed = morsed + ' .-.-. ' # AR: end of message
-        elif i + 1 < len(msg) and msg[i+1] == ' ':
-            pass
-    print(morsed)
-    TIME_UNIT = 0.5
-    for c in morsed:
-        if c == '-':
-            automationhat.light.comms.write(1)
-            time.sleep(TIME_UNIT * 3)
-            automationhat.light.comms.write(0)
-            time.sleep(TIME_UNIT * 1)
-        elif c == '.':
-            automationhat.light.comms.write(1)
-            time.sleep(TIME_UNIT * 1)
-            automationhat.light.comms.write(0)
-            time.sleep(TIME_UNIT * 1)
-        elif c == '_':
-            automationhat.light.comms.write(0)
-            time.sleep(TIME_UNIT * 3)
-        elif c == ' ':
-            automationhat.light.comms.write(0)
-            time.sleep(TIME_UNIT * 7)
+class Schedulable:
+    def __init__(self, when, fn):
+        self.utime = when
+        self.run = fn
 
-if automationhat.is_automation_hat():
-    pass
-else:
-    raise ValueError("no automationhat")
+class Schedule:
+    def __init__(self):
+        self.queue = []
+    
+    def add(self, schedable):
+        if len(self.queue) > 0:
+            assert(self.queue[-1].utime <= schedable.utime)
+        self.queue.append(schedable)
+    
+    def then_after(self, seconds, what):
+        if len(self.queue) > 0:
+            prev = self.queue[-1].utime
+        else:
+            prev = time.time()
+        when = prev + seconds
+        schedable = Schedulable(when, what)
+        self.add(schedable)
+    
+    def next_second(self, what):
+        when = int(time.time()) + 1
+        schedable = Schedulable(when, what)
+        self.add(schedable)
+    
+class Scheduler:
+    def __init__(self):
+        self.schedules = set()
+    
+    def tick(self):
+        cur_utime = time.time()
+        print("tick: " + str(cur_utime))
+        next_utime = math.inf
+        for schedule in self.schedules:
+            for schedulable in schedule.queue:
+                if schedulable.utime < cur_utime:
+                    schedulable.run()
+                else:
+                    if schedulable.utime < next_utime:
+                        next_utime = schedulable.utime
+        return next_utime
+    
+    def loop(self):
+        while True:
+            automationhat.light.power.write(1)
+            next_utime = self.tick()
+            if next_time == math.inf:
+                return
+            cur_utime = time.time()
+            sleep = next_utime - cur_utime
+            if sleep > 0.1:
+                # heartbeat
+                time.sleep(0.05)
+
+            # go to sleep and loop
+            cur_utime = time.time()
+            sleep = next_utime - cur_utime
+            print("Sleeping for " +  str(sleep))
+            automationhat.light.power.write(0)
+            time.sleep(sleep)
+    
+    def add(self, schedule):
+        self.schedules.add(schedule)
+    
+class Morse(Schedule):
+    TIME_UNIT = 0.5
+    
+    def __init__(self, scheduler):
+        self.scheduler.add(self)
+        self.prev_s = '>'
+    
+    def on(self):
+        automationhat.light.comms.write(1)
+    def off(self):
+        automationhat.light.comms.write(0)
+
+    def emit_dash(self):
+        if prev_s == '.' or prev_s == '-':
+            self.then_after(TIME_UNIT, self.on) # inter-symbol space
+        elif prev_s = ' ' or prev_s == '_':
+            pass # we already had a space
+        else:
+            raise ValueError("Invalid morse sequence")
+        self.then_after(TIME_UNIT * 3, self.off) # dash
+        self.prev_s = '-'
+
+    def emit_dot(self):
+        if prev_s == '.' or prev_s == '-':
+            self.then_after(TIME_UNIT, self.on) # inter-symbol space
+        elif prev_s = ' ' or prev_s == '_':
+            pass # we already had a space
+        else:
+            raise ValueError("Invalid morse sequence")
+        self.then_after(TIME_UNIT, self.off) # dash
+        self.prev_s = '.'
+    
+    def emit_letter_space(self):
+        if prev_s == '.' or prev_s == '-':
+            self.then_after(TIME_UNIT * 3, self.on)
+        else:
+            raise ValueError("Invalid morse sequence")
+        self.prev_s = '_'
+
+    def emit_word_space(self):
+        if prev_s == '.' or prev_s == '-' or prev_s == '>':
+            self.then_after(TIME_UNIT * 7, self.on)
+        else:
+            raise ValueError("Invalid morse sequence")
+        self.prev_s = ' '
+    
+    def start_message(self):
+        assert prev_s == '>'
+        self.then_after(TIME_UNIT * 7, self.on)
+        self.prev_s = ' '
+    
+    def end_message(self):
+        if prev_s == '.' or prev_s == '-':
+            pass # we already turned off
+        else:
+            raise ValueError("Invalid morse sequence")
+        self.then_after(TIME_UNIT * 7, self.off) # make sure there's a good space between messages
+        self.prev_s = '>'
+
+    def morse(msg):
+        msg = msg.upper()
+        morsed = ""
+        for i in range(0, len(msg)):
+            morsed = morsed + MORSE_CODE_DICT[msg[i]]
+            if i + 1 < len(msg) and msg[i+1] != ' ':
+                morsed = morsed + '_'
+            elif i + 1 >= len(msg):
+                morsed = morsed + ' .-.-.' # AR: end of message
+            elif i + 1 < len(msg) and msg[i+1] == ' ':
+                pass
+        self.emit_morse(morsed)
+    
+    def emit_morse(self, morsed):
+        print(morsed)
+        self.start_message()
+        for c in morsed:
+            if c == '.':
+                self.emit_dot()
+            elif c == '-':
+                self.emit_dash()
+            elif c == '_':
+                self.emit_letter_space()
+            elif c == ' ':
+                self.emit_word_space()
+            else:
+                raise ValueError("Invalid morse sequence: " + morsed)
+        self.end_message()
+
+if __name__=="__main__":
+    if automationhat.is_automation_hat():
+        pass
+    else:
+        raise ValueError("no automationhat")
+    
+    scheduler = Scheduler()
+    morse = Morse(scheduler)
+    morse.morse("start")
+    scheduler.loop()
+    print("Ran out of things to do, exiting")
+    sys.exit(0)
 
 #automationhat.light.on()
+
 morse("ok")
 
 while True:
@@ -68,8 +202,6 @@ while True:
     #if automationhat.is_automation_hat():
     #    automationhat.relay.two.toggle()
     #    automationhat.relay.three.toggle()
-    automationhat.light.power.write(1)
-    time.sleep(0.05)
     now = datetime.datetime.now()
     print(str(now));
     cur_time = now.time()
@@ -81,10 +213,4 @@ while True:
         print("lights off")
         automationhat.relay.one.off()
 
-    # go to sleep and loop
-    cur_utime = time.time()
-    sleep = int(cur_utime + 1) - cur_utime
-    print("Sleeping for " +  str(sleep))
-    automationhat.light.power.write(0)
-    time.sleep(sleep)
 
