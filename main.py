ccf2c00fdc13dae10d68e8023bda1301c2a5968b
Hazel Victoria Campbell <github@orezpraw.com>
2019-03-13 07:23:38 -0600

    web

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 import gevent
+from gevent.wsgi import WSGIServer
 
 import terrarium
+import web
 
 if __name__=="__main__":
     terrarium = terrarium.Terrarium()
     sched_let = gevent.spawn(terrarium.run_forever())
     greenlets = [sched_let]
+    
+    web_server = WSGIServer(('', 5000), web.app)
+    web_let = gevent.spawn(web_server.serve_forever())
+    greenlets.append(web_let)
+    
     gevent.joinall(greenlets)
     CRITICAL("Ran out of things to do, exiting")
     sys.exit(0)
