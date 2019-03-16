46c16082327100e9b97835be93740af65dbb46a3
Hazel Victoria Campbell <github@orezpraw.com>
2019-03-16 09:17:05 -0600

    persistent storage

--- storage.py
+++ storage.py
@@ -25,35 +25,76 @@ WARNING = logger.warning
 ERROR = logger.error
 CRITICAL = logger.critical
 
+class StorageObserver:
+    def __init__(self, storage, name, observable):
+        self.name = name
+        self.storage = storage
+        self.observable = observable
+        c = self.storage.conn.cursor()
+        command = (
+            "CREATE TABLE IF NOT EXISTS "
+            + name
+            + " (time real)"
+            )
+        c.execute(command)
+        
+        json = self.observable.json()
+        for k, v in json.items():
+            col = k + " "
+            if isinstance(v, str):
+                col = col + "text"
+            elif isinstance(v, int) or isinstance(v, float):
+                col = col + "real"
+            command = "ALTER TABLE " + name + " ADD COLUMN " + col
+            try:
+                c.execute(command)
+            except sqlite3.OperationalError as e:
+                if 'duplicate column' in str(e):
+                    pass
+                else:
+                    raise e
+        self.conn.commit()
+        self.observable.observe(self)
+        self.refresh()
+    
+    def store(self, e):
+        c = self.storage.conn.cursor()
+        cols = []
+        vals = []
+        placeholders = []
+        for col, val in e.items():
+            cols.append(col)
+            vals.append(val)
+            placeholds.append("?")
+        command = (
+            "INSERT INTO " + self.name 
+            + " (" + ", ".join(cols) + ") "
+            + "VALUES (" + ", ".join(placeholders) + ")"
+            )
+        c.execute(command, vals)
+        self.storage.conn.commit()
+        
+    
+    def notify(self, e):
+        if isinstance(e, list):
+            for i in e:
+                self.store(i)
+        else:
+            self.store(e)
+    
+    def refresh(self):
+        self.observable.refresh(self)
+
+
+
 class Storage:
     def create(self):
-        c = self.conn.cursor()
         for name, o in self.observables.items():
-            command = (
-                "CREATE TABLE IF NOT EXISTS "
-                + name
-                + " (time real)"
-                )
-            c.execute(command)
-            
-            json = o.json()
-            for k, v in json.items():
-                col = k + " "
-                if isinstance(v, str):
-                    col = col + "text"
-                elif isinstance(v, int) or isinstance(v, float):
-                    col = col + "real"
-                command = "ALTER TABLE " + name + " ADD COLUMN " + col
-                try:
-                    c.execute(command)
-                except sqlite3.OperationalError as e:
-                    if 'duplicate column' in str(e):
-                        pass
-                    else:
-                        raise e
+            observer = StorageObserver(self, name, o)
             
         
     def __init__(self, observables):
         self.observables = observables
+        self.observers = []
         self.conn = sqlite3.connect('terrarium.sqlite3')
         self.create()
