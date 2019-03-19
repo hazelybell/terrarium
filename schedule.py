6de8abf8269cd205962c9605960a73804c487f83
Hazel Victoria Campbell <github@orezpraw.com>
2019-03-19 09:50:43 -0600

    refactor

--- schedule.py
+++ schedule.py
@@ -15,6 +15,10 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import time
+from gevent import sleep
+
+
 class Schedulable:
     def __init__(self, when, fn):
         assert isinstance(when, float) or isinstance(when, int)
--- soil_moisture.py
+++ soil_moisture.py
@@ -15,6 +15,8 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import time
+
 from observable import Observable
 from schedule import Poller
 
